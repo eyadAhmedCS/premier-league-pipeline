c88070f23cb424c56a35bc44c8a5b60512ad7428
Eyad Ahmed <eyadahmedcs1@gmail.com>
2026-07-08 18:18:31 +0300

    change the order of pipline class methods

--- src/pipeline.py
+++ src/pipeline.py
@@ -7,10 +7,11 @@ print("\n---------------Ectract proccess have started---------------")
 extract.run()
 print("Ectract seasons proccess have ended")
 
+print('\n---------------Transforming process started---------------')
+transform.run()
+print('Transforming process finished')
+
 print('\n---------------Loading  process started---------------')
 load.run()
 print('Loading  process finished')
 
-print('\n---------------Transforming process started---------------')
-transform.run()
-print('Transforming process finished')
